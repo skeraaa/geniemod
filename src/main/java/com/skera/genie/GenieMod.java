fd27df1aebafba8e9943efff1a6732cee70faa4e
skeraaa <skeratekruchin@gmail.com>
2026-04-03 23:12:56 +0300

    Update GenieMod.java

--- src/main/java/com/skera/genie/GenieMod.java
+++ src/main/java/com/skera/genie/GenieMod.java
@@ -3,6 +3,9 @@ package com.skera.genie;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.skera.genie.item.ModItems;
+import com.skera.genie.network.WishHandler;
+import com.skera.genie.config.WishConfig;
 
 public class GenieMod implements ModInitializer {
 	public static final String MOD_ID = "genie";
@@ -10,12 +13,9 @@ public class GenieMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing Genie Mod!");
-		
-		// Register items
-		ModItems.register();
-		
-		// Register network packets
-		ModPackets.register();
+		LOGGER.info("Genie Mod initialized!");
+		ModItems.registerItems();
+		WishHandler.registerPackets();
+		WishConfig.loadWishes(); // Загрузка JSON при старте
 	}
 }
