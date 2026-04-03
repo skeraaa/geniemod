25b431e3ce190fc1039d31332542b4f32a83bea2
skeraaa <skeratekruchin@gmail.com>
2026-04-03 23:13:36 +0300

    Update WishConfig.java

--- src/main/java/com/skera/genie/config/WishConfig.java
+++ src/main/java/com/skera/genie/config/WishConfig.java
@@ -1,11 +1,8 @@
 package com.skera.genie.config;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import net.minecraft.util.Identifier;
 import com.skera.genie.GenieMod;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -14,79 +11,45 @@ import java.util.Map;
 public class WishConfig {
 	private static final Map<String, Wish> WISHES = new HashMap<>();
 	private static final Gson GSON = new Gson();
-	
+
 	public static void loadWishes() {
-		// Загрузка желаний из JSON файлов в resources/data/genie/wishes/
-		// В реальной реализации нужно использовать ResourceManager
+		// Упрощенная загрузка для примера. В продакшене использовать ResourceManager
+		try {
+			InputStream stream = GenieMod.class.getClassLoader().getResourceAsStream("data/genie/wishes/wishes.json");
+			if (stream != null) {
+				JsonObject json = JsonParser.parseReader(new InputStreamReader(stream)).getAsJsonObject();
+				for (String key : json.keySet()) {
+					Wish wish = GSON.fromJson(json.get(key), Wish.class);
+					wish.id = key;
+					WISHES.put(key, wish);
+				}
+				GenieMod.LOGGER.info("Loaded {} wishes.", WISHES.size());
+			}
+		} catch (Exception e) {
+			GenieMod.LOGGER.error("Failed to load wishes!", e);
+		}
 	}
-	
+
 	public static Wish getWish(String id) {
 		return WISHES.get(id);
 	}
-	
-	public static void registerWish(String id, Wish wish) {
-		WISHES.put(id, wish);
-	}
-	
+
 	public static class Wish {
-		public String id;
 		public String category;
 		public String displayName;
 		public float baseRisk;
 		public EffectData effect;
 		public EffectData trickyEffect;
-		
-		public Wish(String id, String category, String displayName, float baseRisk, EffectData effect, EffectData trickyEffect) {
-			this.id = id;
-			this.category = category;
-			this.displayName = displayName;
-			this.baseRisk = baseRisk;
-			this.effect = effect;
-			this.trickyEffect = trickyEffect;
-		}
 	}
-	
+
 	public static class EffectData {
-		public String type; // "item", "effect", "teleport", "spawn_mob"
+		public String type; // item, effect, vampire, fake_block, custom_item, flight, god_mode
 		public String itemId;
 		public int count;
-		public int effectId;
-		public int duration;
+		public String effectId; // Используем строковый ID (minecraft:speed)
+		public int duration; // в тиках
 		public int amplifier;
-		public double x, y, z;
-		public String mobId;
-		
-		public static EffectData item(String itemId, int count) {
-			EffectData data = new EffectData();
-			data.type = "item";
-			data.itemId = itemId;
-			data.count = count;
-			return data;
-		}
-		
-		public static EffectData effect(int effectId, int duration, int amplifier) {
-			EffectData data = new EffectData();
-			data.type = "effect";
-			data.effectId = effectId;
-			data.duration = duration;
-			data.amplifier = amplifier;
-			return data;
-		}
-		
-		public static EffectData teleport(double x, double y, double z) {
-			EffectData data = new EffectData();
-			data.type = "teleport";
-			data.x = x;
-			data.y = y;
-			data.z = z;
-			return data;
-		}
-		
-		public static EffectData spawnMob(String mobId) {
-			EffectData data = new EffectData();
-			data.type = "spawn_mob";
-			data.mobId = mobId;
-			return data;
-		}
+		public String customName;
+		public JsonObject data; // Доп данные для сложных эффектов
 	}
 }
